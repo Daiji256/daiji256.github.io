e79e6ecc47337cc0b4ea8f58d248f983b8ef625b
Daiji256 <daiji.szk@gmail.com>
2024-10-25 19:12:49 +0900

    colors を直接利用

--- astro.config.mjs
+++ astro.config.mjs
@@ -52,7 +52,7 @@ export default defineConfig({
 		css: {
 			preprocessorOptions: {
 				scss: {
-					additionalData: '@import "src/styles/variables.scss";',
+					additionalData: '@use "src/styles/colors.scss"; @import "src/styles/variables.scss";',
 				},
 			},
 		},
