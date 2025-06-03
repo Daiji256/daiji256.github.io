b253cfb853a09761e29da36a274a331d893f087f
Daiji256 <daiji.szk@gmail.com>
2025-06-03 20:51:07 +0900

    Qiita の記事を全部追加

--- src/externalPosts.ts
+++ src/externalPosts.ts
@@ -8,9 +8,161 @@ export const EXTERNAL_POSTS = [
     tags: [
       { id: 'c', name: 'C' },
       { id: 'bit', name: 'bit' },
-      { id: 'bitboard-ja', name: 'ビットボード' },
-      { id: 'bitboard', name: 'bitboard' },
+      { id: 'bitboard', name: 'ビットボード' },
     ],
     url: 'https://qiita.com/Daiji256/items/c4f83c4f3a73a3fb7e0c',
+  },
+  {
+    platform: 'Qiita',
+    title: 'LuaLaTeX でフォント細かく変更する方法（MS Word は使いたくない！）',
+    description:
+      'はじめに課題や予稿，論文等ではフォント等のフォーマット指定がよくあります。MS Word のテンプレートしか無かったり，MS Mincho などの古いフォントを指定されるとイライラします。そこで …',
+    publishedDate: '2021-02-02',
+    tags: [
+      { id: 'tex', name: 'TeX' },
+      { id: 'latex', name: 'LaTeX' },
+      { id: 'lualatex', name: 'LuaLaTeX' },
+      { id: 'font', name: 'Font' },
+    ],
+    url: 'https://qiita.com/Daiji256/items/29adc574b15345d8afa2',
+  },
+  {
+    platform: 'Qiita',
+    title: 'C 言語で eval 風関数',
+    description:
+      'はじめにeval 関数とは文字列を式として評価する関数です。eval は C 言語などのコンパイラ言語にはほとんどないですが，JavaScript などのインタプリタ言語で一般的です。ここでは C…',
+    publishedDate: '2021-02-03',
+    tags: [
+      { id: 'c', name: 'C' },
+      { id: 'eval', name: 'eval' },
+    ],
+    url: 'https://qiita.com/Daiji256/items/09fa042e94907a232c57',
+  },
+  {
+    platform: 'Qiita',
+    title: 'LuaLaTeX のすゝめ',
+    description:
+      'はじめにこの記事では高機能でイケイケな LuaLaTeX を紹介します。現在日本で一般的に使用されている LaTeX は upLaTeX だと思います。upLaTeX は Unicode 対応の…',
+    publishedDate: '2021-02-07',
+    tags: [
+      { id: 'latex', name: 'LaTeX' },
+      { id: 'tex', name: 'TeX' },
+      { id: 'lualatex', name: 'LuaLaTeX' },
+    ],
+    url: 'https://qiita.com/Daiji256/items/9afbfa9f822629d3b995',
+  },
+  {
+    platform: 'Qiita',
+    title: 'LaTeX のすゝめ（2021 年）',
+    description:
+      'はじめに本記事はレポート・論文等の執筆に役立つツールの LaTeX を紹介します。非 LaTeX ユーザや初心者を対象としています。中級者や上級者には物足りないと思います。TeX / LaTeX…',
+    publishedDate: '2021-02-14',
+    tags: [
+      { id: 'latex', name: 'LaTeX' },
+      { id: 'tex', name: 'TeX' },
+      { id: 'beginner', name: '初心者' },
+      { id: 'report', name: 'レポート' },
+      { id: 'uplatex', name: 'upLaTeX' },
+    ],
+    url: 'https://qiita.com/Daiji256/items/9774df0899728feb3ab0',
+  },
+  {
+    platform: 'Qiita',
+    title: '超高速な数式表示（KaTeX より高速！）',
+    description:
+      'この記事は静的サイトジェネレータ向けの内容です。もちろん動的なサイトでも使えますが，おすすめしません。はじめに僕のブログは静的サイトジェネレータの HUGO で作成しています。ここでは HUGO…',
+    publishedDate: '2021-11-20',
+    tags: [
+      { id: 'mathjax', name: 'MathJax' },
+      { id: 'hugo', name: 'Hugo' },
+      { id: 'katex', name: 'KaTeX' },
+      { id: 'tex2svg', name: '`tex2svg`' },
+    ],
+    url: 'https://qiita.com/Daiji256/items/47808405713dc2b33ab1',
+  },
+  {
+    platform: 'Qiita',
+    title: '約組フォント / Yaku Kumi Fonts - 約物専用 Web フォント',
+    description:
+      '概要約物専用の Web フォント「約組フォント / Yaku Kumi Fonts」を公開しました。全角の約物（句読点や括弧）が連続したときのアキを調整する Web フォントです。Android …',
+    publishedDate: '2021-11-20',
+    tags: [
+      { id: 'font', name: 'Font' },
+      { id: 'web-fonts', name: 'Web Fonts' },
+      { id: 'cdn', name: 'CDN' },
+      { id: 'noto-sans', name: 'Noto Sans' },
+      { id: 'punctuation-mark', name: '約物' },
+    ],
+    url: 'https://qiita.com/Daiji256/items/a0a30725355a9f7d6d22',
+  },
+  {
+    platform: 'Qiita',
+    title: 'CuPy を使った画像の幾何学変換 with OpenCV',
+    description:
+      'はじめに魚眼カメラで撮影した等距離射影方式の画像を，普通のカメラの中心射影方式のへの変換をするなどの，画像を幾何学的変換することがあります。僕は研究で，中央付近を大きく，外側を小さくする変換をする…',
+    publishedDate: '2021-12-01',
+    tags: [
+      { id: 'python', name: 'Python' },
+      { id: 'image-processing', name: '画像処理' },
+      { id: 'cuda', name: 'CUDA' },
+      { id: 'opencv', name: 'OpenCV' },
+      { id: 'cupy', name: 'CuPy' },
+    ],
+    url: 'https://qiita.com/Daiji256/items/7fc08524533dc564e844',
+  },
+  {
+    platform: 'Qiita',
+    title: 'PokéAPI を使ってポケモン図鑑の Android を作ってみた',
+    description:
+      'この記事は，フラー株式会社 Advent Calendar 2021 の 9 日目の記事です。8 日目の記事は @inoriko711 さんによる きらめく星（の力で憧れの私）描くよ でした。は…',
+    publishedDate: '2021-12-09',
+    tags: [
+      { id: 'android', name: 'Android' },
+      { id: 'kotlin', name: 'Kotlin' },
+      { id: 'compose', name: 'Compose' },
+      { id: 'pokemon', name: 'ポケモン' },
+    ],
+    url: 'https://qiita.com/Daiji256/items/45461e08f5924ff36c4f',
+  },
+  {
+    platform: 'Qiita',
+    title: 'Android で日本語でも色々なウェイトのフォントを使いたい',
+    description:
+      'この記事は、フラー株式会社のカレンダー | Advent Calendar 2023 - Qiita の 9 日目の記事です^backstory。8 日目は いのりこ (id:inoriko711) さんで…',
+    publishedDate: '2023-12-09',
+    tags: [
+      { id: 'android', name: 'Android' },
+      { id: 'font', name: 'Font' },
+      { id: 'noto-sans', name: 'Noto Sans' },
+    ],
+    url: 'https://qiita.com/Daiji256/items/4b807353c1ca55a26a26',
+  },
+  {
+    platform: 'Qiita',
+    title: 'rehype-mathml: TeX 数式を MathML にする rehype プラグイン',
+    description:
+      'この記事は、フラー株式会社 Advent Calendar 2024 の 7 日目の記事です。6 日目は su8 のサポート終了間近 しがない 30 代エンジニアの本棚（2024 年版）でした。は…',
+    publishedDate: '2024-12-07',
+    tags: [
+      { id: 'tex', name: 'TeX' },
+      { id: 'mathml', name: 'MathML' },
+      { id: 'unified', name: 'unified' },
+      { id: 'rehype', name: 'rehype' },
+      { id: 'remark-math', name: '`remark-math`' },
+    ],
+    url: 'https://qiita.com/Daiji256/items/fcd8ee6f286e5db775d5',
+  },
+  {
+    platform: 'Qiita',
+    title: 'Android アプリにおけるインスタンスの生存期間とスコープ',
+    description:
+      '本記事では、Android アプリ開発において登場する各種オブジェクト（Application、Android、ViewModel など）のインスタンス生存期間とスコープについて整理する。各オブジェ…',
+    publishedDate: '2025-03-05',
+    tags: [
+      { id: 'android', name: 'Android' },
+      { id: 'lifecycle', name: 'Lifecycle' },
+      { id: 'hilt', name: 'Hilt' },
+    ],
+    url: 'https://qiita.com/Daiji256/items/c39f565eade4563ffa43',
   },
 ];
