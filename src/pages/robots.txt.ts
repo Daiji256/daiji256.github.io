f7d37c7b8a6c78dcfed7767f902baa12569813f4
Daiji256 <daiji.szk@gmail.com>
2025-06-10 19:10:56 +0900

    sitemap の path を修正

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -9,6 +9,6 @@ Sitemap: ${sitemapURL.href}
 `.trimStart();
 
 export const GET: APIRoute = ({ site }) => {
-  const sitemapURL = new URL('sitemap-index.xml', site);
+  const sitemapURL = new URL('sitemap.xml', site);
   return new Response(getRobotsTxt(sitemapURL));
 };
