bf6b17977e7e09d2be31028977aa50023e5a346e
Daiji256 <daiji.szk@gmail.com>
2026-03-19 12:46:08 +0900

    Remove spaces between Japanese and English words in Qiita titles and descriptions

--- src/activities.ts
+++ src/activities.ts
@@ -96,9 +96,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'bit を高速に数える 手法の比較',
+    title: 'bitを高速に数える 手法の比較',
     description:
-      'はじめての投稿なので温かい目で見てください。はじめに本記事では bit の 1 の数を数える手法（アルゴリズム）の比較を行います。アルゴリズムの詳しい内容等は紹介せずあくまで処理速度の比較だけ行…',
+      'はじめての投稿なので温かい目で見てください。 はじめに 本記事ではbitの1の数を数える手法（アルゴリズム）の比較を行います。アルゴリズムの詳しい内容等は紹介せずあくまで処理速度の比較だけ行います。アルゴリズムの詳しい内容は下の参考文献に飛んでください。 アルゴリズム ...',
     publishedDate: '2020-12-31T09:58:03+09:00',
     tags: [
       { id: 'c', name: 'C' },
@@ -109,9 +109,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'LuaLaTeX でフォント細かく変更する方法（MS Word は使いたくない！）',
+    title: 'LuaLaTeXでフォント細かく変更する方法（MS Wordは使いたくない！）',
     description:
-      'はじめに課題や予稿，論文等ではフォント等のフォーマット指定がよくあります。MS Word のテンプレートしか無かったり，MS Mincho などの古いフォントを指定されるとイライラします。そこで …',
+      'はじめに 課題や予稿，論文等ではフォント等のフォーマット指定がよくあります。MS Wordのテンプレートしか無かったり，MS Minchoなどの古いフォントを指定されるとイライラします。そこでLaTeXで書きたいと思います。ここではエンジンにLuaLaTeXを使用します。...',
     publishedDate: '2021-02-02T20:12:31+09:00',
     tags: [
       { id: 'tex', name: 'TeX' },
@@ -123,9 +123,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'C 言語で eval 風関数',
+    title: 'C言語でeval風関数',
     description:
-      'はじめにeval 関数とは文字列を式として評価する関数です。eval は C 言語などのコンパイラ言語にはほとんどないですが，JavaScript などのインタプリタ言語で一般的です。ここでは C…',
+      'はじめに eval 関数とは文字列を式として評価する関数です。eval はC言語などのコンパイラ言語にはほとんどないですが，JavaScriptなどのインタプリタ言語で一般的です。ここではC言語のための文字列を数式として処理する eval 風関数を作成します。ソースコード...',
     publishedDate: '2021-02-03T19:54:24+09:00',
     tags: [
       { id: 'c', name: 'C' },
@@ -135,9 +135,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'LuaLaTeX のすゝめ',
+    title: 'LuaLaTeXのすゝめ',
     description:
-      'はじめにこの記事では高機能でイケイケな LuaLaTeX を紹介します。現在日本で一般的に使用されている LaTeX は upLaTeX だと思います。upLaTeX は Unicode 対応の…',
+      'はじめに この記事では高機能でイケイケな LuaLaTeX を紹介します。 現在日本で一般的に使用されている LaTeX は upLaTeX だと思います。upLaTeX は Unicode 対応の日本語 LaTeX エンジンが収録されている，pLaTeX の進化系みたい...',
     publishedDate: '2021-02-07T21:06:21+09:00',
     tags: [
       { id: 'latex', name: 'LaTeX' },
@@ -148,9 +148,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'LaTeX のすゝめ（2021 年）',
+    title: 'LaTeXのすゝめ（2021年）',
     description:
-      'はじめに本記事はレポート・論文等の執筆に役立つツールの LaTeX を紹介します。非 LaTeX ユーザや初心者を対象としています。中級者や上級者には物足りないと思います。TeX / LaTeX…',
+      'はじめに 本記事はレポート・論文等の執筆に役立つツールのLaTeXを紹介します。非LaTeXユーザや初心者を対象としています。中級者や上級者には物足りないと思います。 TeX / LaTeXとは TeX（テック / テフ）1は，スタンフォード大学の計算機科学と数学の教授...',
     publishedDate: '2021-02-14T15:20:45+09:00',
     tags: [
       { id: 'latex', name: 'LaTeX' },
@@ -163,9 +163,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: '超高速な数式表示（KaTeX より高速！）',
+    title: '超高速な数式表示（KaTeXより高速！）',
     description:
-      'この記事は静的サイトジェネレータ向けの内容です。もちろん動的なサイトでも使えますが，おすすめしません。はじめに僕のブログは静的サイトジェネレータの HUGO で作成しています。ここでは HUGO…',
+      'この記事は静的サイトジェネレータ向けの内容です。もちろん動的なサイトでも使えますが，おすすめしません。 はじめに 僕のブログは静的サイトジェネレータのHUGOで作成しています。ここではHUGO用の説明をしますが，他の静的サイトジェネレータ（Hexo，Jekyllなど）でも...',
     publishedDate: '2021-11-20T13:22:57+09:00',
     tags: [
       { id: 'mathjax', name: 'MathJax' },
@@ -177,9 +177,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: '約組フォント / Yaku Kumi Fonts - 約物専用 Web フォント',
+    title: '約組フォント / Yaku Kumi Fonts - 約物専用Webフォント',
     description:
-      '概要約物専用の Web フォント「約組フォント / Yaku Kumi Fonts」を公開しました。全角の約物（句読点や括弧）が連続したときのアキを調整する Web フォントです。Android …',
+      '概要 約物専用のWebフォントの約組フォント / Yaku Kumi Fontsを公開しました。全角の約物（句読点や括弧のこと。）が連続したときのアキを調整するWebフォントです。notofonts/noto-cjkのgoogle-fontsにある NotoSansJP[...',
     publishedDate: '2021-11-20T21:44:27+09:00',
     tags: [
       { id: 'font', name: 'Font' },
@@ -192,9 +192,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'CuPy を使った画像の幾何学変換 with OpenCV',
+    title: 'CuPyを使った画像の幾何学変換with OpenCV',
     description:
-      'はじめに魚眼カメラで撮影した等距離射影方式の画像を，普通のカメラの中心射影方式のへの変換をするなどの，画像を幾何学的変換することがあります。僕は研究で，中央付近を大きく，外側を小さくする変換をする…',
+      'はじめに 魚眼カメラで撮影した等距離射影方式の画像を，普通のカメラの中心射影方式のへの変換をするなどの，画像を幾何学的変換することがあります。僕は研究で，中央付近を大きく，外側を小さくする変換をするために使いました。 ここでは，等距離射影方式の画像を中心射影方式の画像に変...',
     publishedDate: '2021-12-01T16:38:06+09:00',
     tags: [
       { id: 'python', name: 'Python' },
@@ -207,9 +207,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'PokéAPI を使ってポケモン図鑑の Android を作ってみた',
+    title: 'PokéAPIを使ってポケモン図鑑のAndroidを作ってみた',
     description:
-      'この記事は，フラー株式会社 Advent Calendar 2021 の 9 日目の記事です。8 日目の記事は @inoriko711 さんによる きらめく星（の力で憧れの私）描くよ でした。は…',
+      'この記事は，フラー株式会社 Advent Calendar 2021の9日目の記事です。 8日目の記事は@inoriko711さんによるきらめく星（の力で憧れの私）描くよでした。 はじめに 僕はフラー株式会社でAndroidエンジニアのアルバイトのオンボーディング（研修）...',
     publishedDate: '2021-12-09T07:00:00+09:00',
     tags: [
       { id: 'android', name: 'Android' },
@@ -221,9 +221,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'Android で日本語でも色々なウェイトのフォントを使いたい',
+    title: 'Androidで日本語でも色々なウェイトのフォントを使いたい',
     description:
-      'この記事は、フラー株式会社のカレンダー | Advent Calendar 2023 - Qiita の 9 日目の記事です^backstory。8 日目は いのりこ (id:inoriko711) さんで…',
+      'この記事は、フラー株式会社のカレンダー | Advent Calendar 2023 - Qiitaの9日目の記事です1。 8日目はいのりこ (id:inoriko711) さんで弊社エンジニアリンググループ初、産休取得してみたでした。 はじめに デザインの観点から、複数...',
     publishedDate: '2023-12-09T00:08:05+09:00',
     tags: [
       { id: 'android', name: 'Android' },
@@ -234,9 +234,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'rehype-mathml: TeX 数式を MathML にする rehype プラグイン',
+    title: 'rehype-mathml: TeX数式をMathMLにするrehypeプラグイン',
     description:
-      'この記事は、フラー株式会社 Advent Calendar 2024 の 7 日目の記事です。6 日目は su8 のサポート終了間近 しがない 30 代エンジニアの本棚（2024 年版）でした。は…',
+      'この記事は、フラー株式会社 Advent Calendar 2024の7日目の記事です。6日目はsu8のサポート終了間近 しがない30代エンジニアの本棚（2024年版）でした。 はじめに unified（remark、rehype）を使ってMarkdownをHTMLへ変換...',
     publishedDate: '2024-12-07T00:05:15+09:00',
     tags: [
       { id: 'tex', name: 'TeX' },
@@ -249,9 +249,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'Android アプリにおけるインスタンスの生存期間とスコープ',
+    title: 'Androidアプリにおけるインスタンスの生存期間とスコープ',
     description:
-      '本記事では、Android アプリ開発において登場する各種オブジェクト（Application、Android、ViewModel など）のインスタンス生存期間とスコープについて整理する。各オブジェ…',
+      '本記事では、Androidアプリ開発において登場する各種オブジェクト（Application、Android、ViewModel など）のインスタンス生存期間とスコープについて整理する。各オブジェクトがどのタイミング・スコープで生成され、破棄されるのかを理解することで、状態...',
     publishedDate: '2025-03-05T16:21:00+09:00',
     tags: [
       { id: 'android', name: 'Android' },
@@ -262,10 +262,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title:
-      'Navigation Compose における ViewModel での引数の受け取り方 4 選と比較',
+    title: 'Navigation ComposeにおけるViewModelでの引数の受け取り方4選と比較',
     description:
-      'はじめに Navigation Compose 2.8.0 以降では、シリアル化可能なクラスを用いて、型安全に画面間でデータを渡せるようになりました。 本記事では、「画面遷移時の引数を ViewModel でどの受け取るか」に焦点を当て、4 つの方法を紹介し比較します。す...',
+      'はじめに Navigation Compose 2.8.0以降では、シリアル化可能なクラスを用いて、型安全に画面間でデータを渡せるようになりました。 本記事では、「画面遷移時の引数を ViewModel でどの受け取るか」に焦点を当て、4つの方法を紹介し比較します。すべて...',
     publishedDate: '2025-06-14T09:40:18+09:00',
     tags: [
       { id: 'android', name: 'Android' },
@@ -278,9 +277,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: '簡単で安全な UriHandler の実装例と利用方法',
+    title: '簡単で安全なUriHandlerの実装例と利用方法',
     description:
-      'UriHandler とは UriHandler は androidx.compose.ui.platform が提供する、URI（Web ページ、ファイル、外部アプリなど）を開くためのインターフェースです。Composable 関数からは、以下のようにシンプルに呼び出せ...',
+      'UriHandler とは UriHandler は androidx.compose.ui.platform が提供する、URI（Webページ、ファイル、外部アプリなど）を開くためのインターフェースです。Composable関数からは、以下のようにシンプルに呼び出せます...',
     publishedDate: '2025-06-14T09:40:18+09:00',
     tags: [
       { id: 'android', name: 'Android' },
@@ -293,9 +292,9 @@ export const ACTIVITIES = [
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
     title:
-      'LocalSnackbarHostState: CompositionLocal による SnackbarHostState の管理方法',
+      'LocalSnackbarHostState: CompositionLocalによるSnackbarHostStateの管理方法',
     description:
-      'モバイルアプリにおいてユーザーへの情報伝達は非常に重要です。その中でも、短時間でユーザーにフィードバックを伝える際に便利な UI 要素として Snackbar があります。 この記事では、Compose における Snackbar の基本から、画面などのスコープを意識した ...',
+      'モバイルアプリにおいてユーザーへの情報伝達は非常に重要です。その中でも、短時間でユーザーにフィードバックを伝える際に便利なUI要素としてSnackbarがあります。 この記事では、ComposeにおけるSnackbarの基本から、画面などのスコープを意識したSnackbar...',
     publishedDate: '2025-06-28T13:03:28+09:00',
     tags: [
       { id: 'android', name: 'Android' },
@@ -308,9 +307,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'mapState / combineState: StateFlow を同期処理で変換・合成する',
+    title: 'mapState / combineState: StateFlowを同期処理で変換・合成する',
     description:
-      'Android アプリ開発において、StateFlow は UI 状態管理に不可欠です。しかし、実際の開発では以下の課題に直面することがあります： StateFlow から別の StateFlow への変換 複数の StateFlow を組み合わせた StateFlow...',
+      'Androidアプリ開発において、StateFlow はUI状態管理に不可欠です。しかし、実際の開発では以下の課題に直面することがあります： StateFlow から別の StateFlow への変換 複数の StateFlow を組み合わせた StateFlow の作...',
     publishedDate: '2025-06-28T20:09:03+09:00',
     tags: [
       { id: 'android', name: 'Android' },
@@ -323,10 +322,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title:
-      'EntryPoint を使った Composable 関数への依存性注入 with Hilt & Compose',
+    title: 'EntryPointを使ったComposable関数への依存性注入with Hilt & Compose',
     description:
-      'Composable 関数内で、Hilt によって @Provides や @Binds で提供されたオブジェクトを直接的に扱いたい場面があります。 通常、Hilt を用いた依存性注入は ViewModel を経由して行います。たとえば @HiltViewModel に依存...',
+      'Composable関数内で、Hiltによって @Provides や @Binds で提供されたオブジェクトを直接的に扱いたい場面があります。 通常、Hiltを用いた依存性注入はViewModelを経由して行います。たとえば @HiltViewModel に依存性注入し、...',
     publishedDate: '2025-07-12T10:36:59+09:00',
     tags: [
       { id: 'android', name: 'Android' },
@@ -340,9 +338,9 @@ export const ACTIVITIES = [
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
     title:
-      'ユーザーの選んだスタイルを尊重する：Android アプリでのシステムスタイル取得',
+      'ユーザーの選んだスタイルを尊重する：Androidアプリでのシステムスタイル取得',
     description:
-      'はじめに Android には、ユーザーが自分の好みや特性に合わせて設定できるシステムスタイルがあります。テーマ（ダークモード）、フォントスケール（フォントサイズ）、Material You（システムカラー）、色のコントラストなどが代表例です。これらはユーザーの「見やすさ...',
+      'はじめに Androidには、ユーザーが自分の好みや特性に合わせて設定できるシステムスタイルがあります。テーマ（ダークモード）、フォントスケール（フォントサイズ）、Material You（システムカラー）、色のコントラストなどが代表例です。これらはユーザーの「見やすさ」...',
     publishedDate: '2025-09-10T16:55:57+09:00',
     tags: [
       { id: 'android', name: 'Android' },
@@ -356,9 +354,9 @@ export const ACTIVITIES = [
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
     title:
-      'Swift/Objective-C から Kotlin へインスタンスを渡すと deinit が遅れる理由と対策',
+      'Swift/Objective-CからKotlinへインスタンスを渡すとdeinitが遅れる理由と対策',
     description:
-      '本記事は、Kotlin Advent Calendar 2025 の 17 日目の記事です。 はじめに Kotlin Multiplatform（KMP）で iOS アプリを開発する際、Swift と Kotlin の相互運用は避けて通れません。KMP は非常に便利ですが...',
+      '本記事は、Kotlin Advent Calendar 2025の17日目の記事です。 はじめに Kotlin Multiplatform（KMP）でiOSアプリを開発する際、SwiftとKotlinの相互運用は避けて通れません。KMPは非常に便利ですが、トラブルなく使い...',
     publishedDate: '2025-12-17T16:11:24+09:00',
     tags: [
       { id: 'kotlin-multiplatform', name: 'Kotlin Multiplatform' },
@@ -371,10 +369,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title:
-      'GitHub Actions で orphan ブランチを使ってファイルを保存・取得する方法',
+    title: 'GitHub Actionsでorphanブランチを使ってファイルを保存・取得する方法',
     description:
-      '本記事は、GitHub Actions Advent Calendar 2025 の 19 日目の記事です。 はじめに GitHub Actions で生成されたファイルを保存する代表的な手段として、GitHub Actions の Artifacts や、S3 などの外...',
+      '本記事は、GitHub Actions Advent Calendar 2025の19日目の記事です。 はじめに GitHub Actionsで生成されたファイルを保存する代表的な手段として、GitHub ActionsのArtifactsや、S3などの外部ストレージサー...',
     publishedDate: '2025-12-19T19:04:32+09:00',
     tags: [
       { id: 'github-actions', name: 'GitHub Actions' },
@@ -387,9 +384,9 @@ export const ACTIVITIES = [
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
     title:
-      'Kotlin Contracts の InvocationKind.EXACTLY_ONCE を「1 回呼び出す」と誤解してはいけない',
+      'Kotlin ContractsのInvocationKind.EXACTLY_ONCEを「1回呼び出す」と誤解してはいけない',
     description:
-      'はじめに Kotlin には Contracts という、関数の内部動作に関するヒントをコンパイラに提供する機能があります。これにより、コンパイラはスマートキャストや変数の初期化判定をより賢く行えるようになります。 通常、コンパイラは関数の内部の実装まで深く解析しませんが...',
+      'はじめに KotlinにはContractsという、関数の内部動作に関するヒントをコンパイラに提供する機能があります。これにより、コンパイラはスマートキャストや変数の初期化判定をより賢く行えるようになります。 通常、コンパイラは関数の内部の実装まで深く解析しませんが、co...',
     publishedDate: '2026-01-14T21:31:48+09:00',
     tags: [
       { id: 'kotlin', name: 'Kotlin' },
@@ -399,9 +396,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'SnapshotStateList の toString / equals / hashCode の罠',
+    title: 'SnapshotStateListのtoString / equals / hashCodeの罠',
     description:
-      'はじめに Compose で List を扱うとき、mutableStateListOf で生成される SnapshotStateList を利用することは多いでしょう。 しかし SnapshotStateList の toString / equals / hashCo...',
+      'はじめに Composeで List を扱うとき、mutableStateListOf で生成される SnapshotStateList を利用することは多いでしょう。 しかし SnapshotStateList の toString / equals / hashCod...',
     publishedDate: '2026-02-19T00:31:57+09:00',
     tags: [
       { id: 'android', name: 'Android' },
@@ -413,9 +410,9 @@ export const ACTIVITIES = [
   },
   {
     platform: { name: 'Qiita', url: 'https://qiita.com/Daiji256' },
-    title: 'Nav3 は状態の保持と描画を分けて考えると設計しやすい',
+    title: 'Nav3は状態の保持と描画を分けて考えると設計しやすい',
     description:
-      'はじめに Jetpack Navigation 3（以下 Nav3）は柔軟性が高い一方で、実装方針が 1 つに定まっているわけではありません。そのため、実運用ではアプリの要件に応じて設計する場面が多く、実装が複雑になりやすいです。 本記事では、Nav3 において状態の保持...',
+      'はじめに Jetpack Navigation 3（以下Nav3）は柔軟性が高い一方で、実装方針が1つに定まっているわけではありません。そのため、実運用ではアプリの要件に応じて設計する場面が多く、実装が複雑になりやすいです。 本記事では、Nav3において状態の保持と描画を...',
     publishedDate: '2026-03-14T20:47:16+09:00',
     tags: [
       { id: 'android', name: 'Android' },
