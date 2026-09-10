c1e650569b436b9eec809fe1aea7ac5570b297bf
Daiji256 <daiji.szk@gmail.com>
2026-09-10 12:20:46 +0900

    fix: resolve missing post titles in RSS feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,18 +1,21 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import type { APIContext } from 'astro';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function GET(context) {
-  const posts = await getCollection('posts');
+export async function GET(context: APIContext) {
+  const posts = (await getCollection('posts')).sort(
+    (a, b) => b.data.publishedDate.getTime() - a.data.publishedDate.getTime(),
+  );
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: context.site,
+    site: context.site!,
     items: posts.map((post) => ({
-      title: post.title,
+      title: post.data.title,
       link: `/posts/${post.id}/`,
       description: post.data.description,
-      pubDate: post.data.publishedDate.toISOString(),
+      pubDate: post.data.publishedDate,
     })),
   });
 }
